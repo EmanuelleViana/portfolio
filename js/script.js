4aa36d76e63e4c7078eba92e8c4c1a415fa3dfd3
EmanuelleViana <emanuelleve@hotmail.com>
2020-08-26 14:04:32 -0300

    feature: paginação na listagem de  projetos

---
+++ js/script.js
@@ -0,0 +1,6 @@
+new SimpleSlide({
+  slide: 'projects', // nome do atributo data-slide="principal"
+  nav: true, // se deve ou não mostrar a navegação
+  auto: false, // se o slide deve passar automaticamente
+  time: 5000, // tempo de transição dos slides
+});
